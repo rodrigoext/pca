2b758fcb24ffe0497d99a9fbd2f228f21c307eae
Rodrigo Exterkoetter <rodrigo.exterkoetter@hexagonagriculture.com>
2016-06-16 15:41:54 -0300

    Finish compute method.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,11 @@
 #include <Eigen/Dense>
 
 int main() {
-	std::cout << "Init PCA..." << std::endl;
 	PCA pca;
 	Eigen::MatrixXf data(2, 2);
-	data << 1, 0,
+	data << 2, 0,
 			0, 1;
 	Eigen::VectorXf r = pca.Compute(data);
-	std::cout << "Result: " << std::endl << r << std::endl;
+	std::cout << "Result: " << std::endl << r.transpose() << std::endl;
 	return 0;
 }
--- pca.cpp
+++ pca.cpp
@@ -1,4 +1,5 @@
 #include "pca.h"
+#include <iostream>
 
 PCA::PCA()
 {
@@ -7,8 +8,27 @@ PCA::PCA()
 
 Eigen::VectorXf PCA::Compute(const Eigen::MatrixXf &data)
 {
-	Eigen::VectorXf result(10);
-	return result;
+	// Compute the d-dimensional mean vector
+	Eigen::VectorXf means = data.colwise().mean();
+
+	// Subtract mean image from the data set to get mean centered data vector
+	Eigen::MatrixXf U = data.rowwise() - means.transpose();
+
+	// Covariance matrix from the mean centered data matrix
+	Eigen::MatrixXf covariance = (U.transpose() * U) / (float)(data.rows());
+
+	// Calculate the eigenvalues and eigenvectors for the covariance matrix
+	Eigen::EigenSolver<Eigen::MatrixXf> solver(covariance);
+	Eigen::MatrixXf eigen_vectors = solver.eigenvectors().real();
+	Eigen::VectorXf eigen_values = solver.eigenvalues().real();
+
+	eigen_vectors.normalize();
+
+	std::sort(eigen_values.derived().data(), eigen_values.derived().data() + eigen_values.derived().size());
+    short index = eigen_values.size() - 1;
+    Eigen::VectorXf feature_vector = eigen_vectors.row(index);
+
+    return feature_vector;
 }
 
 PCA::~PCA()
